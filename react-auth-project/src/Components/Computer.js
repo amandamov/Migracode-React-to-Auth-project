cb005cc63537b5ba69a8627e68060cea56b0da58
Amanda Maria <amanda.moov@gmail.com>
2022-12-09 02:13:23 +0100

    update homepage

---
+++ react-auth-project/src/Components/Computer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import computer from "src/img/computer.jpg"
+
+const Computer = () => {
+    return (
+        <div>
+            <img src={computer} alt="Computer photo" height={200} width={200}/>
+        </div>
+    );
+}
+
+export default Computer;
--- react-auth-project/src/pages/Home.jsx
+++ react-auth-project/src/pages/Home.jsx
@@ -1,8 +1,37 @@
 import React from "react";
+import {Routes, Route, useNavigate, Navigate} from 'react-router-dom';
+import Login from "./Login";
+import Sign_up from "./Sign-up";
+
+
 
 function Home() {
+
+    const navigate = useNavigate();
+
+    const navigateToLogin = () => {
+        navigate('/login')
+    }
+
+    const navigateToSignUp = () => {
+        navigate('/sign-up')
+    }
+
     return (
-        <h1>this is the homepage</h1>
+        <div className="container-home">
+            <h1>Welcome to the Authentication Project</h1>
+            <h2>A cool project to practice the contents learned in Migracode classes.</h2>
+            <h3>We've learned about Node.js / API and now bundling with React. </h3>
+            <p>Please, wish me luck!</p>
+            {/* <img src={require('./img/computer.jpg').default} alt="Computer photo" height={200} width={200}/> */}
+            <button onClick={navigateToLogin} className="button-login">Login</button>
+            <button onClick={navigateToSignUp}>Sign-up</button>
+
+            <Routes>
+                <Route path='login' element={<Login />} />
+                <Route path='sign-up' element={<Sign_up />} />
+            </Routes>
+        </div>
     )
 }
 
